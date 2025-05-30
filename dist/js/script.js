47452a10cda58a72684850e3d84c9bf98e97ac8d
Michał Puchała <michalpuchala86@gmail.com>
2025-05-30 20:49:21 +0200

    Add images to clicked option

--- dist/js/script.js
+++ dist/js/script.js
@@ -33,14 +33,14 @@
     },
   };
 
-  /*const classNames = {
+  const classNames = {
     menuProduct: {
       wrapperActive: "active",
       imageVisible: "active",
     },
   };
 
-  const settings = {
+  /*const settings = {
     amountWidget: {
       defaultValue: 1,
       defaultMin: 0,
@@ -67,24 +67,17 @@
     }
     renderInMenu() {
       const thisProduct = this;
-      const generatedHTML = templates.menuProduct(
-        thisProduct.data
-      ); /*generate HTML based on template*/
-      thisProduct.element =
-        utils.createDOMFromHTML(
-          generatedHTML
-        ); /*create element using utils.createElementFromHTML*/
-      const menuContainer = document.querySelector(
-        select.containerOf.menu
-      ); /*find menu container*/
+      const generatedHTML = templates.menuProduct(thisProduct.data); /*generate HTML based on template*/
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML); /*create element using utils.createElementFromHTML*/
+      const menuContainer = document.querySelector(select.containerOf.menu); /*find menu container*/
       menuContainer.appendChild(thisProduct.element); /*add element to menu*/
     }
     getElements() {
       const thisProduct = this;
 
-      thisProduct.accordionTrigger = thisProduct.element.querySelector(
-        select.menuProduct.clickable
-      );
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       thisProduct.form = thisProduct.element.querySelector(
         select.menuProduct.form
       );
@@ -119,9 +112,7 @@
             activeProduct.classList.remove(select.all.menuProductsActive);
           } /* if there is active product and it's not thisProduct.element, remove class active from it */
 
-        thisProduct.element.classList.toggle(
-          "active"
-        ); /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle("active"); /* toggle active class on thisProduct.element */
       });
     }
     initOrderForm() {
@@ -142,7 +133,7 @@
         event.preventDefault();
         thisProduct.processOrder();
       });
-      console.log("metoda = initOrderForm");
+      //console.log("metoda = initOrderForm");
     }
 
     processOrder() {
@@ -150,7 +141,7 @@
 
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log("formData", formData);
+      //console.log("formData", formData);
 
       // set price to default price
       let price = thisProduct.data.price;
@@ -159,29 +150,34 @@
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log(
-          "to co jest w srodku",
-          paramId,
-          "to co jest w param",
-          param
-        );
+        //console.log("to co jest w srodku", paramId,"to co jest w param",param);
 
         // for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+          //console.log(optionId, option);
 
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if (formData[paramId] && formData[paramId].includes(optionId)) {
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          if (optionSelected) {
             // check if the option is not default
             if (!option.default) {
               price += option.price; // add option price to price variable
             }
           } else {
-            // check if the option is default
-            if (option.default) {
+              // check if the option is default
+              if (option.default) {
               price -= option.price; // reduce price variable
+              }
+            }
+          const optionImage = thisProduct.imageWrapper.querySelector(optionId,paramId);
+          if(optionImage) {
+            if(optionSelected) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            }
+            else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
             }
           }
         }
@@ -217,5 +213,6 @@
       thisApp.initMenu();
     },
   };
+
   app.init();
 }
--- src/js/script.js
+++ src/js/script.js
@@ -33,14 +33,14 @@
     },
   };
 
-  /*const classNames = {
+  const classNames = {
     menuProduct: {
       wrapperActive: "active",
       imageVisible: "active",
     },
   };
-  // eslint-disable-line
-  const settings = {
+
+  /*const settings = {
     amountWidget: {
       defaultValue: 1,
       defaultMin: 0,
@@ -74,6 +74,8 @@
     }
     getElements() {
       const thisProduct = this;
+
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
 
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       thisProduct.form = thisProduct.element.querySelector(
@@ -131,7 +133,7 @@
         event.preventDefault();
         thisProduct.processOrder();
       });
-      console.log("metoda = initOrderForm");
+      //console.log("metoda = initOrderForm");
     }
 
     processOrder() {
@@ -139,7 +141,7 @@
 
       // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
       const formData = utils.serializeFormToObject(thisProduct.form);
-      console.log("formData", formData);
+      //console.log("formData", formData);
 
       // set price to default price
       let price = thisProduct.data.price;
@@ -148,24 +150,34 @@
       for (let paramId in thisProduct.data.params) {
         // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
         const param = thisProduct.data.params[paramId];
-        console.log("to co jest w srodku", paramId,"to co jest w param",param);
+        //console.log("to co jest w srodku", paramId,"to co jest w param",param);
 
         // for every option in this category
         for (let optionId in param.options) {
           // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
           const option = param.options[optionId];
-          console.log(optionId, option);
+          //console.log(optionId, option);
 
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if (formData[paramId] && formData[paramId].includes(optionId)) {
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          if (optionSelected) {
             // check if the option is not default
             if (!option.default) {
               price += option.price; // add option price to price variable
             }
           } else {
-            // check if the option is default
-            if (option.default) {
+              // check if the option is default
+              if (option.default) {
               price -= option.price; // reduce price variable
+              }
+            }
+          const optionImage = thisProduct.imageWrapper.querySelector(optionId,paramId);
+          if(optionImage) {
+            if(optionSelected) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            }
+            else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
             }
           }
         }
@@ -201,5 +213,6 @@
       thisApp.initMenu();
     },
   };
+
   app.init();
 }
